576ede984e2f8c4f8945a4d804648c8289b15ccf
manishaac2002 <manishaac2002@gmail.com>
2024-03-05 16:45:12 +0530

    feat :add up the comments

--- middleware/logger.js
+++ middleware/logger.js
@@ -6,7 +6,7 @@ const logger = createLogger({
   transports: [
     new transports.File({ //log of an api 
       filename: 'user.log',// this is a file get store the logs
-      level: 'info',//
+      level: 'info',//level of logging message 
       format: format.combine(format.timestamp(), format.json())
     }), new transports.File({ //if error occurs api logs store here
       filename: 'user-error.log',
