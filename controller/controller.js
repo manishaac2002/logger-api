d348ac4ef60a018d015c0b8d785ca00ec250de68
manishaac2002 <manishaac2002@gmail.com>
2024-03-06 12:38:30 +0530

    fix : to be fix the api url to be logged.

--- controller/controller.js
+++ controller/controller.js
@@ -4,8 +4,8 @@ import apiLogger from '../middleware/logger.js';
 //get admin details
 const getAdminDetailsController = (request, response) => {
   response.send('Hello World')
- const triggeredUrl =request.url
-  // apiLogger.logger.log('info','Successfully got logged',triggeredUrl)
+ const triggerApiLogger = apiLogger.logger.log('info','Successfully got logged')
+ 
 }
 
 //crete new admin
