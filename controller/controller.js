96fd14d1f13d0e57ac0d92ade2a54792be7c42a1
manishaac2002 <manishaac2002@gmail.com>
2024-03-06 12:12:17 +0530

    fix : to be fix the api url to be logged.

--- controller/controller.js
+++ controller/controller.js
@@ -4,7 +4,8 @@ import apiLogger from '../middleware/logger.js';
 //get admin details
 const getAdminDetailsController = (request, response) => {
   response.send('Hello World')
-  apiLogger.logger.log('info','Successfully got logged')
+ const triggeredUrl =request.url
+  // apiLogger.logger.log('info','Successfully got logged',triggeredUrl)
 }
 
 //crete new admin
--- middleware/logger.js
+++ middleware/logger.js
@@ -2,12 +2,13 @@ import { createLogger, transports, format } from 'winston'
 
 // logging function
 const logger = createLogger({
-
+ 
   transports: [
     new transports.File({ //log of an api 
       filename: 'user.log',// this is a file get store the logs
       level: 'info',//level of logging message 
       format: format.combine(format.timestamp(), format.json())
+      
     }), new transports.File({ //if error occurs api logs store here
       filename: 'user-error.log',
       level: 'error',
@@ -15,6 +16,7 @@ const logger = createLogger({
     })
   ]
 });
+
 
 export default {
   logger
