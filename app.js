78e55407998f65ee40ab72983d80c4ddabefe9af
manishaac2002 <manishaac2002@gmail.com>
2024-04-27 14:03:28 +0530

    chore --

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-import express from "express";
-import controller from './controller.js';
-import winston from "./middleware/winston.js";
+import express from "express"
+import controller from './controller.js'
+import winston from "./middleware/winston.js"
 
-const application = express();
+const application = express()
 
 // Middleware to log API requests
 application.use((request, response, next) => {
-  winston.apiLogger(request, response, next);
-});
+  winston.apiLogger(request, response, next)
+})
 
 // Define routes
-application.get('/getMessages', controller.getMessages);
+application.get('/getMessages', controller.getMessages)
 
 const port = 6001;
 application.listen(port, () => {
-  console.log(`Server is running on the port ${port}`);
+  console.log(`Server is running on the port ${port}`)
 });
--- controller.js
+++ controller.js
@@ -1,13 +1,7 @@
-import winston from "./middleware/winston.js";
-
 const getMessages = (request, response) => {
-  // Call the apiLogger middleware
-  // winston.apiLogger(request, response, () => {
-  // This is the route handler logic
-  response.send('Hello World');
-  // });
-};
+   response.send('Hello World');
+}
 
 export default {
   getMessages
-};
+}
