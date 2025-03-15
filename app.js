971cb5924ca2fbfb81b588646eb88b9945671a13
manishac2002 <manishaac2002@gmail.com>
2025-03-15 15:01:31 +0530

    feat : add an api .

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ application.get("/user", (req, res) => {
   res.send("I'm user");
 });
 
+application.get("/employee", (req, res) => {
+  res.send(" Employee details");
+});
+
+
 application.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
